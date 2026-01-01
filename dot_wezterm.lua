1cd4346c57a0ded50e9d7c44f76acd14a173d98c
Rick <137560494+rcopra@users.noreply.github.com>
2026-01-01 22:34:19 +0100

    Initial chezmoi dotfiles migration

---
+++ dot_wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 17
+
+config.keys = {
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
+	{
+		key = "k",
+		mods = "CMD",
+		action = act.Multiple({
+			act.ClearScrollback("ScrollbackAndViewport"),
+			act.SendKey({ key = "L", mods = "CTRL" }),
+		}),
+	},
+	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-1) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(1) },
+}
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+	},
+}
+
+config.window_decorations = "RESIZE"
+
+-- config.colors = {
+-- 	foreground = "#CBE0F0",
+-- 	background = "#011423",
+-- 	cursor_bg = "#47FF9C",
+-- 	cursor_border = "#47FF9C",
+-- 	cursor_fg = "#011423",
+-- 	selection_bg = "#033259",
+-- 	selection_fg = "#CBE0F0",
+-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
+
+config.color_scheme = "Kanagawa (Gogh)"
+
+return config
