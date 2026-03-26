e0b23155c0353396a040575c58ae9e197423ce59
rcopra <137560494+rcopra@users.noreply.github.com>
2026-03-26 11:44:39 +0100

    new colors

--- home/dot_fzf.zsh
+++ home/dot_fzf.zsh
@@ -4,13 +4,12 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
-# Gruvbox Material Mix (Hard) colors
+# Catppuccin Mocha colors
 export FZF_DEFAULT_OPTS="
-  --color=fg:#e2cca9,bg:#1d2021,hl:#e9b143
-  --color=fg+:#e2cca9,bg+:#3c3836,hl+:#e9b143
-  --color=border:#504945,header:#80aa9e,gutter:#1d2021
-  --color=spinner:#f28534,info:#8bba7f
-  --color=pointer:#d3869b,marker:#f2594b,prompt:#928374
+  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+  --color=selected-bg:#45475a
 "
 
 # Use fzf shell integration
